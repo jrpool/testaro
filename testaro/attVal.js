39e07d4fdaff83306750169e4ce92240e3ba0de4
Jonathan Pool <pool@jpdev.pro>
2025-12-06 16:39:29 -0800

    refactor attVal to eliminate sampling

--- testaro/attVal.js
+++ testaro/attVal.js
@@ -24,35 +24,44 @@
 
 /*
   attVal
-  This test reports attributes with illicit values.
+  This test reports elements with illicit values of an attribute.
 */
 
-// ########## IMPORTS
-
-// Module to perform common operations.
-const {init, getRuleResult} = require('../procs/testaro');
-
-// ########## FUNCTIONS
+// FUNCTIONS
 
 // Runs the test and returns the result.
 exports.reporter = async (page, withItems, attributeName, areLicit, values) => {
-  // Initialize the locators and result.
-  const all = await init(100, page, `[${attributeName}]`);
-  // For each locator:
-  for (const loc of all.allLocs) {
-    // Get whether its element violates the rule.
-    const value = await loc.getAttribute(attributeName);
-    const isBad = areLicit !== values.includes(value);
-    // If it does:
-    if (isBad) {
-      // Add the locator to the array of violators.
-      all.locs.push([loc, value]);
+  // Return totals and standard instances for the rule.
+  return await page.evaluate(args => {
+    const [withItems, attributeName, areLicit, values] = args;
+    // Get all candidates, i.e. elements with the attribute.
+    const candidates = document.body.querySelectorAll(`[${attributeName}]`);
+    let violationCount = 0;
+    const instances = [];
+    // For each candidate:
+    candidates.forEach(element => {
+      const value = element.getAttribute(attributeName);
+      // If it violates the rule:
+      if (areLicit !== values.includes(value)) {
+        // Increment the violation count.
+        violationCount++;
+        // If itemization is required:
+        if (withItems) {
+          const what = `Element has attribute ${attributeName} with illicit value ${value}`;
+          instances.push(window.getInstance(element, 'attVal', what, 1, 2));
+        }
+      }
+    });
+    // If there were any violations and itemization is not required:
+    if (violationCount && ! withItems) {
+      const what = `Elements have attribute ${attributeName} with illicit values`;
+      // Add a summary instance to the instances.
+      instances.push(window.getInstance(null, 'attVal', what, violationCount, 2));
     }
-  }
-  // Populate and return the result.
-  const whats = [
-    `Element has attribute ${attributeName} with illicit value “__param__”`,
-    `Elements have attribute ${attributeName} with illicit values`
-  ];
-  return await getRuleResult(withItems, all, 'attVal', whats, 2);
+    return {
+      data: {},
+      totals: [0, 0, violationCount, 0],
+      standardInstances: instances
+    };
+  }, [withItems, attributeName, areLicit, values]);
 };
