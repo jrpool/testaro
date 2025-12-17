f8c23c7a7f076d7fe368b7c2bfaaddea483fd158
Jonathan Pool <pool@jpdev.pro>
2025-12-17 15:02:43 -0800

    refactor role test to eliminate sampling and use aria-query package

--- testaro/role.js
+++ testaro/role.js
@@ -15,75 +15,27 @@
 
 // IMPORTS
 
-// Module to perform common operations.
-const {init, getRuleResult} = require('../procs/testaro');
+const {elementRoles} = require('aria-query');
+const {doTest} = require('../procs/testaro');
 
 // CONSTANTS
 
-  // Implicit roles
-  const roleImplications = {
-    article: 'article',
-    aside: 'complementary',
-    button: 'button',
-    datalist: 'listbox',
-    dd: 'definition',
-    details: 'group',
-    dfn: 'term',
-    dialog: 'dialog',
-    dt: 'term',
-    fieldset: 'group',
-    figure: 'figure',
-    h1: 'heading',
-    h2: 'heading',
-    h3: 'heading',
-    h4: 'heading',
-    h5: 'heading',
-    h6: 'heading',
-    hr: 'separator',
-    html: 'document',
-    'input[type=number]': 'spinbutton',
-    'input[type=text]': 'textbox',
-    'input[type=text, list]': 'combobox',
-    li: 'listitem',
-    main: 'main',
-    math: 'math',
-    menu: 'list',
-    nav: 'navigation',
-    ol: 'list',
-    output: 'status',
-    progress: 'progressbar',
-    summary: 'button',
-    SVG: 'graphics-document',
-    table: 'table',
-    tbody: 'rowgroup',
-    textarea: 'textbox',
-    tfoot: 'rowgroup',
-    thead: 'rowgroup',
-    tr: 'row',
-    ul: 'list'
-  };
-  const implicitRoles = new Set(Object.values(roleImplications));
+// Implicit roles
+const implicitRoles = new Set(Array.from(elementRoles.values()).flat());
 
 // FUNCTIONS
 
 // Runs the test and returns the result.
 exports.reporter = async (page, withItems) => {
-  // Get locators for all elements with explicit roles.
-  const all = await init(100, page, '[role]');
-  // For each locator:
-  for (const loc of all.allLocs) {
+  const getBadWhat = element => {
     // Get the explicit role of the element.
-    const role = await loc.getAttribute('role');
+    const role = element.getAttribute('role');
     // If it is also implicit:
     if (implicitRoles.has(role)) {
-      // Add the locator to the array of violators.
-      all.locs.push([loc, role]);
+      // Return a violation description.
+      return `Explicit ${role} role of the element is also an implicit HTML element role`;
     }
-  }
-  // Populate and return the result.
-  const whats = [
-    'Element has an explicit __param__ role, which is also an implicit HTML element role',
-    'Elements have roles assigned that are also implicit roles of HTML elements'
-  ];
-  return await getRuleResult(withItems, all, 'role', whats, 0);
+  };
+  const whats = 'Elements have roles assigned that are also implicit HTML element roles';
+  return await doTest(page, withItems, 'role', whats, 0, null, getBadWhat.toString());
 };
