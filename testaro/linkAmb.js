762f235f51753da9806b607b68314621266bd2d9
Jonathan Pool <pool@jpdev.pro>
2025-12-16 20:07:10 -0800

    continue refactoring linkAmb test

--- testaro/linkAmb.js
+++ testaro/linkAmb.js
@@ -37,7 +37,7 @@ exports.reporter = async (page, withItems) => {
     const linkData = {};
     // For each visible link on the page:
     visibleLinks.forEach(link => {
-      const text = link.textContent.trim();
+      const text = link.textContent.trim().toLowerCase();
       linkData[text] ??= {
         linkCount: 0,
         hrefCount: 0,
@@ -51,108 +51,32 @@ exports.reporter = async (page, withItems) => {
         linkData[text].hrefs[href] = true;
       }
     });
-    // Get data on texts shared by links that have at least 2 different destinations.
+    // Get the texts of violator links and the destination counts of the texts.
     const violationEntries = Object.entries(linkData).filter((entry => entry[1].hrefCount > 1));
+    const textData = {};
     violationEntries.forEach(entry => {
-      const text = entry[0];
-      const data = entry[1];
-
-    })
-
-    .map(([text, data]) => page.locator(`a:text("${text}")`));
+      textData[entry[0]] = entry[1].hrefCount;
+    });
+    // Return the data.
+    return textData;
   });
-  const allLocs = Object.entries(violationData).filter(([text, hrefs]) => Object.keys(hrefs).length > 1).map(([text, hrefs]) => page.locator(`a:text("${text}")`));
+  // Get locators for all visible links.
+  const linksLoc = page.locator('a[href]:visible');
+  const linkLocs = await linksLoc.all();
   const violations = [];
-  const data = {};
-  // For each locator:
-  for (const loc of allLocs) {
-    // Get the XPath of the element referenced by the locator.
-    let xPath = await loc.evaluate(element => getXPath(element));
-    const pathSegments = xPath.split('/');
-    const {length} = pathSegments;
-    // Change it to the XPath of the desired observation root.
-    pathSegments.pop();
-    if (! ['main', 'body'].includes(pathSegments[length - 2])) {
-      pathSegments.pop();
-    }
-    xPath = pathSegments.join('/');
-    // Get a locator for the observation root.
-    const rootLoc = page.locator(`xpath=${xPath}`);
-    const loc0 = await rootLoc.locator('*:visible');
-    // Get a count of the visible elements in the observation tree.
-    const elementCount0 = await loc0.count();
-    try {
-      // Hover over the element.
-      await loc.hover({timeout: 400});
-      // Get the change in the count of the visible elements in the observation tree.
-      const changeData = await getVisibleCountChange(rootLoc, elementCount0, 400, 75);
-      const {change, elapsedTime} = changeData;
-      // If a change occurred:
-      if (change) {
-        // Add the locator and a violation description to the array of violations.
-        violations.push({
-          loc,
-          what: getViolationDescription(change, elapsedTime)
-        });
-      }
-      // Stop hovering over the element.
-      await page.mouse.move(0, 0);
-      // Await a reverse change in the count of the visible elements in the observation tree.
-      await getVisibleCountChange(rootLoc, elementCount0 + change);
-    }
-    // If hovering throws an error:
-    catch(error) {
-      // Report that the test was prevented.
-      data.prevented = true;
-      data.error = `ERROR hovering over an element (${error.message})`;
-      break;
+  // For each of them:
+  for (const loc of linkLocs) {
+    const text = (await loc.textContent()).trim().toLowerCase();
+    // If it is a violator:
+    if (violationData[text]) {
+      // Add the locator and a violation description to the array of violations.
+      violations.push({
+        loc,
+        what: `${violationData[text]} links with the same text have different destinations`
+      });
     }
   }
   // Get and return a result.
-  const whats = 'Hovering over elements changes the number of related visible elements';
-  return await getBasicResult(page, withItems, 'hover', 0, '', whats, data, violations);
-};
-
-// Module to perform common operations.
-const {init, getRuleResult} = require('../procs/testaro');
-// Module to get locator data.
-const {getLocatorData} = require('../procs/getLocatorData');
-
-// ########## FUNCTIONS
-
-// Runs the test and returns the result.
-exports.reporter = async (page, withItems) => {
-  // Initialize the locators and result.
-  const all = await init(100, page, 'a[href]:visible');
-  const linksData = [];
-  // For each locator:
-  for (const loc of all.allLocs) {
-    // Get its text.
-    const elData = await getLocatorData(loc);
-    const linkText = elData.excerpt.toLowerCase();
-    // Get its destination.
-    const linkTo = await loc.getAttribute('href');
-    // If the text and destination exist:
-    if (linkText && linkTo) {
-      // If a previous link has the same text but a different destination:
-      if (linksData.some(linkData => linkData.text === linkText && linkData.to !== linkTo)) {
-        // Add the locator to the array of violators.
-        all.locs.push(loc);
-      }
-      // Otherwise, i.e. if no previous link has the same taxt but a different destination:
-      else {
-        // Record its text and destination.
-        linksData.push({
-          text: linkText,
-          to: linkTo
-        });
-      }
-    }
-  }
-  // Populate and return the result.
-  const whats = [
-    'Link has the same text as, but a different destination from, another',
-    'Links have the same texts but different destinations'
-  ];
-  return await getRuleResult(withItems, all, 'linkAmb', whats, 2);
+  const whats = 'Links have the same text but different destinations';
+  return await getBasicResult(page, withItems, 'linkAmb', 2, 'LI', whats, {}, violations);
 };
