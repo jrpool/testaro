f75e8e78427d05f0ea689fc3373700a0948d5630
Jonathan Pool <pool@jpdev.pro>
2025-12-24 15:21:51 -0800

    refactor ibm tool

--- tests/ibm.js
+++ tests/ibm.js
@@ -22,7 +22,6 @@
 
 // IMPORTS
 
-const fs = require('fs').promises;
 // Scanner. Importing and executing 'close' crashed the Node process.
 const accessibilityChecker = require('accessibility-checker');
 const {getCompliance} = accessibilityChecker;
@@ -30,7 +29,7 @@ const {getCompliance} = accessibilityChecker;
 // FUNCTIONS
 
 // Runs the IBM test and returns the result.
-const run = async (content, timeLimit) => {
+const run = async content => {
   const nowLabel = (new Date()).toISOString().slice(0, 19);
   try {
     const ibmReport = await getCompliance(content, nowLabel);
@@ -66,13 +65,13 @@ const limitRuleTotals = (actReport, rules) => {
   }
 };
 // Trims an IBM report.
-const trimActReport = (data, actReport, withItems, rules) => {
+const trimActReport = (actReport, withItems, rules) => {
   // If the act report includes a summary:
   if (actReport && actReport.summary) {
     // Remove excluded rules from the act report.
     limitRuleTotals(actReport, rules);
-    // If the act report includes totals:
     const totals = actReport.summary.counts;
+    // If the act report includes totals:
     if (totals) {
       // If itemization is required:
       if (withItems) {
@@ -101,115 +100,67 @@ const trimActReport = (data, actReport, withItems, rules) => {
     }
     // Otherwise, i.e. if it excludes totals:
     else {
-      // Report this.
-      const error = 'ERROR: No totals reported';
-      console.log(error);
-      data.prevented = true;
-      data.error = error;
-      // Return an empty act report.
+      // Return an act report with this error.
       return {
-        totals: {},
-        items: []
+        totals: null,
+        items: [],
+        error: 'No totals reported'
       };
     }
   }
   // Otherwise, i.e. if it excludes a summary:
   else {
-    // Report this.
-    const error = 'ERROR: No summary reported';
-    console.log(error);
-    data.prevented = true;
-    data.error = error;
-    // Return an empty act report.
+    // Return an act report with this error.
     return {
-      totals: {},
-      items: []
-    };
-  }
-};
-// Performs the IBM tests and returns an act report.
-const doTest = async (content, withItems, timeLimit, rules) => {
-  // Conduct the tests.
-  const data = {};
-  try {
-    const runReport = await run(content, timeLimit);
-    // If there were results:
-    if (runReport.report) {
-      // Return a trimmed act report.
-      const {report} = runReport;
-      const trimmedReport = trimActReport(data, report, withItems, rules);
-      return {
-        data,
-        result: trimmedReport
-      };
-    }
-    // Otherwise, i.e. if there were no results:
-    else {
-      // Return this.
-      return {
-        data: runReport,
-        result: {}
-      }
-    }
-  }
-  catch(error) {
-    const message = `Act failed (${error.message.slice(0, 200)})`;
-    console.log(message);
-    data.prevented = true;
-    data.error = message;
-    return {
-      data,
-      result: {}
+      totals: null,
+      items: [],
+      error: 'No summary reported'
     };
   }
 };
 // Conducts and reports the IBM Equal Access tests.
-exports.reporter = async (page, report, actIndex, timeLimit) => {
+exports.reporter = async (page, report, actIndex) => {
   const act = report.acts[actIndex];
   const {withItems, withNewContent, rules} = act;
   const contentType = withNewContent ? 'new' : 'existing';
   try {
     const typeContent = contentType === 'existing' ? await page.content() : page.url();
-    // Perform the tests.
-    const actReport = await doTest(typeContent, withItems, timeLimit, rules);
-    // If the testing was finished on time:
-    if (actReport !== 'timedOut') {
-      const {data, result} = actReport;
-      // If the act was prevented:
-      if (data && data.prevented) {
-        // Report this.
-        const message = 'ERROR: Act was prevented';
-        data.error ??= message;
-        // Return the failure.
+    // Conduct the tests.
+    const runReport = await run(typeContent);
+    const {report} = runReport;
+    // If there were results:
+    if (report) {
+      // Trim them.
+      const trimmedReport = trimActReport(report, withItems, rules);
+      const {error} = trimmedReport;
+      // If the report was not trimmable:
+      if (error) {
+        // Return an act report with this error.
         return {
-          data,
+          data: {
+            prevented: true,
+            error
+          },
           result: {}
-        };
+        }
       }
-      // Otherwise, i.e. if the act was not prevented:
-      else {
-        // Return the result.
-        return {
-          data,
-          result
-        };
-      }
-    }
-    // Otherwise, i.e. if the testing timed out:
-    else {
-      // Report this.
+      // Otherwise, i.e. if the report was trimmable, return it.
       return {
-        data: {
-          prevented: true,
-          error: message
-        },
-        result: {}
+        data: {},
+        result: trimmedReport
       };
     }
+    // Otherwise, i.e. if there was only an error, return it in an act report.
+    return {
+      data: runReport,
+      result: {}
+    }
   }
+  // If an error occurred:
   catch(error) {
-    const message = `Act crashed (${error.message.slice(0, 200)})`;
-    console.log(`ERROR: ${message}`);
+    const message = `Act failed (${error.message.slice(0, 200)})`;
+    console.log(message);
+    // Return it in an act report.
     return {
       data: {
         prevented: true,
