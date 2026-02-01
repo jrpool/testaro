fb25031b013872a6a64504d947fd8c06098e49c9
Jonathan Pool <pool@jpdev.pro>
2026-01-31 19:53:38 -0800

    debug

--- procs/error.js
+++ procs/error.js
@@ -13,6 +13,15 @@
   Handles errors.
 */
 
+// Reports a job being aborted.
+const abortActs = exports.abortActs = (report, actIndex) => {
+  // Add data on the aborted act to the report.
+  report.jobData.abortTime = nowString();
+  report.jobData.abortedAct = actIndex;
+  report.jobData.aborted = true;
+  // Report that the job is aborted.
+  console.log(`ERROR: Job aborted on act ${actIndex}`);
+};
 // Adds an error result to an act.
 exports.addError = (alsoLog, alsoAbort, report, actIndex, message) => {
   // If the error is to be logged:
@@ -20,18 +29,23 @@ exports.addError = (alsoLog, alsoAbort, report, actIndex, message) => {
     // Log it.
     console.log(message);
   }
-  // Add error data to the result.
   const act = report.acts[actIndex];
-  act.result ??= {};
-  act.result.success ??= false;
-  act.result.error ??= message;
-  if (act.type === 'test') {
-    act.data ??= {};
-    act.data.prevented = true;
-    act.data.error = message;
-    // Add prevention data to the job data.
-    report.jobData.preventions[act.which] = message;
-  }
+  // If an act was specified:
+  if (act) {
+    // Add error data to the result.
+    act.result ??= {};
+    act.result.success ??= false;
+    act.result.error ??= message;
+    // If the act is a test act:
+    if (act.type === 'test') {
+      act.data ??= {};
+      // Add prevention data to the act data.
+      act.data.prevented = true;
+      act.data.error = message;
+      // Add prevention data to the job data.
+      report.jobData.preventions[act.which] = message;
+    }
+  };
   // If the job is to be aborted:
   if (alsoAbort) {
     // Add this to the report.
