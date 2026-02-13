d446202cb3bc146d91ec3b0f14c44c7156f6df00
Jonathan Pool <pool@jpdev.pro>
2026-02-13 08:46:38 -0800

    continue refactoring wave

--- tests/wave.js
+++ tests/wave.js
@@ -43,116 +43,146 @@ exports.reporter = async (page, report, actIndex) => {
   ];
   const query = queryParams.filter(param => param).join('&');
   const path = [wavePath, query].join('?');
-  // Initialize the results.
+  // Initialize the act report.
   const data = {};
-  let result = {};
-  try {
-    result = await new Promise(resolve => {
-      // Make the request.
-      https.get(
-        {
-          host,
-          path
-        },
-        response => {
-          let rawReport = '';
-          response.on('data', chunk => {
-            rawReport += chunk;
-          });
-          // When they arrive:
-          response.on('end', async () => {
-            let actResult = {};
-            // Delete unnecessary properties.
-            try {
-              actResult = JSON.parse(rawReport);
-              const {categories, statistics} = actResult;
-              delete categories.feature;
-              delete categories.structure;
-              delete categories.aria;
-              // If rules were specified:
+  const result = {
+    nativeResult: {},
+    standardResult: {}
+  };
+  const standard = report.standard !== 'no';
+  // If standard results are to be reported:
+  if (standard) {
+    // Initialize the standard result.
+    result.standardResult = {
+      prevented: false,
+      totals: [0, 0, 0, 0],
+      instances: []
+    };
+  }
+  // Make a request to the WAVE API.
+  https.get(
+    {
+      host,
+      path
+    },
+    response => {
+      let rawReport = '';
+      response.on('data', chunk => {
+        rawReport += chunk;
+      });
+      // When the response arrives:
+      response.on('end', async () => {
+        try {
+          // Parse it as JSON.
+          result.nativeResult = JSON.parse(rawReport);
+        }
+        // If it was not parsable:
+        catch (error) {
+          // Report this.
+          data.prevented = true;
+          data.error = error.message;
+          result.nativeResult = {};
+        }
+        // If the response was parsed:
+        if (! data.prevented) {
+          const {nativeResult} = result;
+          const {categories} = nativeResult;
+          // Delete its unnecessary properties.
+          delete categories.feature;
+          delete categories.structure;
+          delete categories.aria;
+          // For each WAVE rule category:
+          ['error', 'contrast', 'alert'].forEach(categoryName => {
+            const category = categories[categoryName];
+            const ordinalSeverity = categoryName === 'alert' ? 0 : 3;
+            // If any violations were reported:
+            if (
+              category?.items
+              && Object.keys(category.items).length
+            ) {
+              const {items} = category;
+              // If rules to be tested for were specified:
               if (rules && rules.length) {
-                // For each WAVE rule category:
-                ['error', 'contrast', 'alert'].forEach(categoryName => {
-                  const category = categories[categoryName];
-                  // If any violations were reported:
-                  if (
-                    category
-                    && category.items
-                    && Object.keys(category.items).length
-                  ) {
-                    const {items} = category;
-                    // For each rule violated:
-                    Object.keys(items).forEach(ruleID => {
-                      // If it was not a specified rule:
-                      if (! rules.includes(ruleID)) {
-                        // Decrease the category violation count by the count of its violations.
-                        category.count -= items[ruleID].count;
-                        // Remove its violations from the report.
-                        delete items[ruleID];
-                      }
-                    });
+                // For each rule violated:
+                Object.keys(items).forEach(ruleID => {
+                  // If it was not a specified rule:
+                  if (! rules.includes(ruleID)) {
+                    // Decrease the category violation count by the count of its violations.
+                    category.count -= items[ruleID].count;
+                    // Remove its violations from the native result.
+                    delete items[ruleID];
                   }
                 });
               }
-              // Get the WAVE WCAG documentation.
-              const waveDocResponse = await fetch('https://wave.webaim.org/api/docs');
-              const waveDoc = waveDocResponse.ok ? await waveDocResponse.json() : [];
-              // For each rule category:
-              for (const categoryName of Object.keys(categories)) {
-                const category = categories[categoryName];
-                // If any violations were reported in the category:
-                if (
-                  category
-                  && category.items
-                  && Object.keys(category.items).length
-                ) {
-                  const {items} = category;
-                  // For each rule violated (named item by WAVE):
-                  for (const ruleName of Object.keys(items)) {
-                    const ruleDoc = waveDoc.find((rule => rule.name === ruleName));
-                    const {guidelines} = ruleDoc;
-                    const rule = items[ruleName];
-                    // Add WCAG information to the rule data.
-                    rule.wcag = guidelines || [];
-                    // For each violation:
-                    for (const index in rule.selectors) {
-                      const selector = rule.selectors[index] || '';
-                      let excerpt = '';
-                      // If a selector is provided:
-                      if (selector) {
-                        // Get an excerpt of the element.
-                        excerpt = await page.evaluate(selector => {
-                          const element = document.querySelector(selector);
-                          // If the selector matches an element:
-                          if (element) {
-                            // Get an excerpt of the element.
-                            const rawExcerpt = element.textContent.trim() || element.outerHTML.trim();
-                            const normalizedExcerpt = rawExcerpt.replace(/\s+/g, ' ');
-                            return normalizedExcerpt.slice(0, 300);
-                          }
-                          else {
-                            return '';
-                          }
-                        }, selector);
+              // If standard results are to be reported:
+              if (standard) {
+                const {standardResult} = result;
+                const {totals, instances} = standardResult;
+                // Add the category violation count to the standard-result totals.
+                totals[ordinalSeverity] += category.count;
+                const pathIDItems = await page.evaluate(items => {
+                  const ruleIDs = Object.keys(items);
+                  // For each rule of the category with any violations:
+                  ruleIDs.forEach(ruleID => {
+                    const {selectors} = items[ruleID];
+                    // For each of those violations:
+                    selectors.forEach(selector => {
+                      // Get the violating element.
+                      const violator = document.querySelector(selector[0]);
+                      // Add its path ID to the violation record.
+                      selector.push(window.getXPath(violator) ?? '');
+                    });
+                  });
+                  return items;
+                }, items);
+                const ruleIDs = Object.keys(pathIDItems);
+                // For each rule of the category with any violations:
+                for (const ruleID of ruleIDs) {
+                  const {description, selectors} = pathIDItems[ruleID];
+                  // For each violation of the rule:
+                  for (const violation of selectors) {
+                    // Initialize a standard instance.
+                    const instance = {
+                      ruleID,
+                      what: description,
+                      ordinalSeverity,
+                      count: 1
+                    };
+                    const pathID = violation[2];
+                    // If the path ID of the violator was found:
+                    if (pathID) {
+                      // Get the catalog index of the violator.
+                      const catalogIndex = getXPathCatalogIndex(pathID);
+                      // If the acquisition succeeded:
+                      if (catalogIndex) {
+                        // Add the catalog index to the instance.
+                        instance.catalogIndex = catalogIndex;
                       }
-                      // Convert the violation selector to a selector-excerpt pair.
-                      rule.selectors[index] = [selector, excerpt];
+                      // Otherwise, i.e. if the acquisition failed:
+                      else {
+                        // Add the path ID to the instance.
+                        instance.pathID = pathID;
+                      }
                     }
+                    // Add the instance to the standard result.
+                    instances.push(instance);
                   }
                 }
-              };
-              // Add important data to the result.
-              if (statistics) {
-                data.pageTitle = statistics.pagetitle || '';
-                data.pageURL = statistics.pageurl || '';
-                data.elapsedSeconds = statistics.time || null;
-                data.creditsRemaining = statistics.creditsremaining || null;
-                data.allItemCount = statistics.allitemcount || null;
-                data.totalElements = statistics.totalelements || null;
-                data.waveURL = statistics.waveurl || '';
               }
-              // Return the result.
-              resolve(actResult);
+            }
+          });
+          // Add important data to the result.
+          if (statistics) {
+            data.pageTitle = statistics.pagetitle || '';
+            data.pageURL = statistics.pageurl || '';
+            data.elapsedSeconds = statistics.time || null;
+            data.creditsRemaining = statistics.creditsremaining || null;
+            data.allItemCount = statistics.allitemcount || null;
+            data.totalElements = statistics.totalelements || null;
+            data.waveURL = statistics.waveurl || '';
+          }
+          // Return the result.
+          resolve(actResult);
             }
             catch(error) {
               data.prevented = true;
