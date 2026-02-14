5d4431033098c10563d1724d5f603ef8efac4ac9
Jonathan Pool <pool@jpdev.pro>
2026-02-13 16:51:30 -0800

    update license statement dates

--- testaro/secHeading.js
+++ testaro/secHeading.js
@@ -1,7 +1,7 @@
 /*
   © 2025 CVS Health and/or one of its affiliates. All rights reserved.
   © 2025 Juan S. Casado.
-  © 2025 Jonathan Robert Pool.
+  © 2025–2026 Jonathan Robert Pool.
 
   Licensed under the MIT License. See LICENSE file at the project root or  https://opensource.org/license/mit/ for details.
 
